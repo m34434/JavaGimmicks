4a746e7fe8ea03e03ddc244c4e2e94ce4fb93a71
michael <m34434@googlemail.com>
2013-09-06 14:41:36 +0200

    Javadoccing and small changes

--- src/main/java/net/sf/javagimmicks/collections/AutoDroppingQueueDecorator.java
+++ src/main/java/net/sf/javagimmicks/collections/AutoDroppingQueueDecorator.java
@@ -5,7 +5,22 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import net.sf.javagimmicks.collections.decorators.AbstractQueueDecorator;
+import net.sf.javagimmicks.swing.BoundedEventQueue;
 
+/**
+ * A decorator for a given {@link Queue} that limits the maximum number of
+ * entries within there to a given number by dropping a given number of entries
+ * from it's end if the internal {@link Queue} is full.
+ * <p>
+ * A use case for this might be processing status messages in a
+ * producer/consumer scenario from a long-running operation where each status
+ * updates the previous one (so the previous one is obsolete and can be
+ * dropped).
+ * <p>
+ * {@link BoundedEventQueue} makes use of this class for processing huge numbers
+ * of Swing UI (component update) events without blocking Swing with too many of
+ * them.
+ */
 public class AutoDroppingQueueDecorator<E> extends AbstractQueueDecorator<E>
 {
    private static final long serialVersionUID = 8775576852585456548L;
@@ -13,6 +28,17 @@ public class AutoDroppingQueueDecorator<E> extends AbstractQueueDecorator<E>
    private final int _maxSize;
    private final int _dropCount;
 
+   /**
+    * Create a new instance around the given {@link Queue} with a given size
+    * limit and drop count.
+    * 
+    * @param decorated
+    *           the {@link Queue} to decorate
+    * @param maxSize
+    *           the size limit for the decorator
+    * @param dropCount
+    *           the numer of entries to drop if the size threshold is reached
+    */
    public AutoDroppingQueueDecorator(final Queue<E> decorated, final int maxSize, final int dropCount)
    {
       super(decorated);
@@ -36,16 +62,41 @@ public class AutoDroppingQueueDecorator<E> extends AbstractQueueDecorator<E>
       _dropCount = dropCount;
    }
 
+   /**
+    * Create a new instance around the given {@link Queue} with a given size
+    * limit that drops all entries when full.
+    * 
+    * @param decorated
+    *           the {@link Queue} to decorate
+    * @param maxSize
+    *           the size limit for the decorator
+    */
    public AutoDroppingQueueDecorator(final Queue<E> internalQueue, final int maxSize)
    {
       this(internalQueue, maxSize, maxSize);
    }
 
+   /**
+    * Create a new instance wrapping an internal {@link LinkedList} with a given
+    * size limit and drop count.
+    * 
+    * @param maxSize
+    *           the size limit for the decorator
+    * @param dropCount
+    *           the numer of entries to drop if the size threshold is reached
+    */
    public AutoDroppingQueueDecorator(final int maxSize, final int skipCount)
    {
       this(new LinkedList<E>(), maxSize, skipCount);
    }
 
+   /**
+    * Create a new instance wrapping an internal {@link LinkedList} with a given
+    * size limit that drops all entries when full.
+    * 
+    * @param maxSize
+    *           the size limit for the decorator
+    */
    public AutoDroppingQueueDecorator(final int maxSize)
    {
       this(maxSize, maxSize);
