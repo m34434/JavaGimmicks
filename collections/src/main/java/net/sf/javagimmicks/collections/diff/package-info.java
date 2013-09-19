9702563599abae19c33f1a9bd3a11ff307453d5f
michael <m34434@googlemail.com>
2013-09-19 23:55:58 +0200

    Refactoring of the diff API

--- collections/src/main/java/net/sf/javagimmicks/collections/diff/package-info.java
+++ collections/src/main/java/net/sf/javagimmicks/collections/diff/package-info.java
@@ -1,4 +1,7 @@
 /**
- * A difference API for Java Collections
+ * A difference API for Java Collections.
+ * <p>
+ * The central entry point is {@link net.sf.javagimmicks.collections.diff.DifferenceUtils}
+ * - this is were you should start.
  */
 package net.sf.javagimmicks.collections.diff;
