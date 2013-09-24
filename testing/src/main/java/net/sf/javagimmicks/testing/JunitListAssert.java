7de332ed495a0e074c06db362032a83b8a3a67c7
m34434 <m34434@googlemail.com>
2013-09-24 12:54:00 +0200

    JavaDoccing

--- testing/src/main/java/net/sf/javagimmicks/testing/JunitListAssert.java
+++ testing/src/main/java/net/sf/javagimmicks/testing/JunitListAssert.java
@@ -4,14 +4,41 @@ import static org.junit.Assert.assertArrayEquals;
 
 import java.util.List;
 
+/**
+ * Contains utility methods the help to make assertions against {@link List}s by
+ * providing the expected values as a variable argument list.
+ */
 public class JunitListAssert
 {
-   public static <E> void assertListEquals(String message, List<? extends E> actual, E... expected)
+   private JunitListAssert()
+   {}
+
+   /**
+    * Compares the given {@link List} against a variable argument list of
+    * expected values.
+    * 
+    * @param message
+    *           the error message to show if the assertion fails
+    * @param actual
+    *           the actual values as {@link List}
+    * @param expected
+    *           the expected values as variable argument list
+    */
+   public static <E> void assertListEquals(final String message, final List<? extends E> actual, final E... expected)
    {
       assertArrayEquals(message, expected, actual.toArray());
    }
 
-   public static <E> void assertListEquals(List<? extends E> actual, E... expected)
+   /**
+    * Compares the given {@link List} against a variable argument list of
+    * expected values.
+    * 
+    * @param actual
+    *           the actual values as {@link List}
+    * @param expected
+    *           the expected values as variable argument list
+    */
+   public static <E> void assertListEquals(final List<? extends E> actual, final E... expected)
    {
       assertArrayEquals(expected, actual.toArray());
    }
