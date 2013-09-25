26454e5caf0cd781a477167213cdeac194ecf58d
m34434 <m34434@googlemail.com>
2013-09-25 16:56:33 +0200

    JavaDoccing

--- core/src/main/java/net/sf/javagimmicks/xml/DOMUtils.java
+++ core/src/main/java/net/sf/javagimmicks/xml/DOMUtils.java
@@ -57,6 +57,14 @@ public class DOMUtils
    private DOMUtils()
    {}
 
+   /**
+    * Retrieves all child {@link Element}s from a given parent {@link Element}
+    * as {@link List}.
+    * 
+    * @param element
+    *           the parent {@link Element}
+    * @return the {@link List} of child {@link Element}s
+    */
    public static List<Element> getChildElements(final Element element)
    {
       final List<Element> result = new ArrayList<Element>();
@@ -74,6 +82,18 @@ public class DOMUtils
       return result;
    }
 
+   /**
+    * Retrieves all child {@link Element}s of a given namespace URI and local
+    * name from a given parent {@link Element} as {@link List}.
+    * 
+    * @param element
+    *           the parent {@link Element}
+    * @param namespaceURI
+    *           the namespace URI of the child {@link Element}s to retrieve
+    * @param localName
+    *           the local name of the child {@link Element}s to retrieve
+    * @return the respective {@link List} of child {@link Element}s
+    */
    public static List<Element> getChildElements(final Element element, final String namespaceURI,
          final String localName)
    {
@@ -102,7 +122,17 @@ public class DOMUtils
       return result;
    }
 
-   public static List<Element> getChildElements(final Element element, final String name)
+   /**
+    * Retrieves all child {@link Element}s of a given local name from a given
+    * parent {@link Element} as {@link List}.
+    * 
+    * @param element
+    *           the parent {@link Element}
+    * @param localName
+    *           the local name of the child {@link Element}s to retrieve
+    * @return the respective {@link List} of child {@link Element}s
+    */
+   public static List<Element> getChildElements(final Element element, final String localName)
    {
       final List<Element> result = new ArrayList<Element>();
 
@@ -113,7 +143,7 @@ public class DOMUtils
             continue;
          }
 
-         if (!name.equals(child.getNodeName()))
+         if (!localName.equals(child.getNodeName()))
          {
             continue;
          }
@@ -124,6 +154,14 @@ public class DOMUtils
       return result;
    }
 
+   /**
+    * Retrieves all {@link Attr Attribute}s from a given parent {@link Element}
+    * as {@link List}.
+    * 
+    * @param element
+    *           the parent {@link Element}
+    * @return the {@link List} of {@link Attr Attribute}s
+    */
    public static List<Attr> getAttributes(final Element element)
    {
       final NamedNodeMap attributes = element.getAttributes();
@@ -144,63 +182,184 @@ public class DOMUtils
       };
    }
 
+   /**
+    * Creates and returns a new {@link DocumentBuilder}.
+    * 
+    * @return a new {@link DocumentBuilder}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
    public static DocumentBuilder newDocumentBuilder() throws ParserConfigurationException
    {
       return _dbf.newDocumentBuilder();
    }
 
+   /**
+    * Creates and returns a new {@link Document}.
+    * 
+    * @return a new {@link Document}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
    public static Document newDocument() throws ParserConfigurationException
    {
       return newDocumentBuilder().newDocument();
    }
 
-   public static Document newDocument(final String nsUri, final String prefix, final String elementName)
+   /**
+    * Creates and returns a new {@link Document} with a root {@link Element}
+    * according to the given parameters.
+    * 
+    * @param nsUri
+    *           the namespace URI for the root {@link Element}
+    * @param prefix
+    *           the prefix for the root {@link Element}
+    * @param localName
+    *           the local name for the root {@link Element}
+    * @return a new according {@link Document}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
+   public static Document newDocument(final String nsUri, final String prefix, final String localName)
          throws ParserConfigurationException
    {
-      final String qualifiedName = (prefix == null ? "" : (prefix + ":")) + elementName;
+      final String qualifiedName = (prefix == null ? "" : (prefix + ":")) + localName;
 
       return getDOMImplementation().createDocument(nsUri, qualifiedName, null);
    }
 
-   public static Document newDocument(final String nsUri, final String elementName)
+   /**
+    * Creates and returns a new {@link Document} with a root {@link Element}
+    * according to the given parameters.
+    * 
+    * @param nsUri
+    *           the namespace URI for the root {@link Element}
+    * @param localName
+    *           the local name for the root {@link Element}
+    * @return a new according {@link Document}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
+   public static Document newDocument(final String nsUri, final String localName)
          throws ParserConfigurationException
    {
-      return newDocument(nsUri, null, elementName);
+      return newDocument(nsUri, null, localName);
    }
 
-   public static Document newDocument(final String elementName) throws ParserConfigurationException
+   /**
+    * Creates and returns a new {@link Document} with a root {@link Element}
+    * according to the given parameters.
+    * 
+    * @param localName
+    *           the local name URI for the root {@link Element}
+    * @return a new according {@link Document}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
+   public static Document newDocument(final String localName) throws ParserConfigurationException
    {
-      return newDocument(null, elementName);
+      return newDocument(null, localName);
    }
 
+   /**
+    * Creates and returns a new {@link DOMImplementation}.
+    * 
+    * @return a new {@link DOMImplementation}
+    * @throws ParserConfigurationException
+    *            if the creation fails
+    */
    public static DOMImplementation getDOMImplementation() throws ParserConfigurationException
    {
       return newDocumentBuilder().getDOMImplementation();
    }
 
+   /**
+    * Parses a new XML {@link Document} from a given {@link InputSource}
+    * 
+    * @param inputSource
+    *           the {@link InputSource} to parse from
+    * @return the parsed {@link Document}
+    * @throws SAXException
+    *            if the XML source is not valid
+    * @throws IOException
+    *            if the source could not be read
+    * @throws ParserConfigurationException
+    *            if the parser could not be created
+    */
    public static Document parse(final InputSource inputSource) throws SAXException, IOException,
          ParserConfigurationException
    {
       return newDocumentBuilder().parse(inputSource);
    }
 
+   /**
+    * Parses a new XML {@link Document} from a given {@link InputStream}
+    * 
+    * @param inputStream
+    *           the {@link InputStream} to parse from
+    * @return the parsed {@link Document}
+    * @throws SAXException
+    *            if the XML source is not valid
+    * @throws IOException
+    *            if the source could not be read
+    * @throws ParserConfigurationException
+    *            if the parser could not be created
+    */
    public static Document parse(final InputStream inputStream) throws SAXException, IOException,
          ParserConfigurationException
    {
       return parse(new InputSource(inputStream));
    }
 
+   /**
+    * Parses a new XML {@link Document} from a given {@link Reader}
+    * 
+    * @param reader
+    *           the {@link Reader} to parse from
+    * @return the parsed {@link Document}
+    * @throws SAXException
+    *            if the XML source is not valid
+    * @throws IOException
+    *            if the source could not be read
+    * @throws ParserConfigurationException
+    *            if the parser could not be created
+    */
    public static Document parse(final Reader reader) throws SAXException, IOException, ParserConfigurationException
    {
       return parse(new InputSource(reader));
    }
 
+   /**
+    * Parses a new XML {@link Document} from a given XML {@link String}.
+    * 
+    * @param xmlContent
+    *           the XML {@link String} to parse from
+    * @return the parsed {@link Document}
+    * @throws SAXException
+    *            if the XML source is not valid
+    * @throws IOException
+    *            if the source could not be read
+    * @throws ParserConfigurationException
+    *            if the parser could not be created
+    */
    public static Document parse(final String xmlContent) throws SAXException, IOException,
          ParserConfigurationException
    {
       return parse(new StringReader(xmlContent));
    }
 
+   /**
+    * Serializes a given XML {@link Node} into a {@link String} with optionally
+    * pretty-printing.
+    * 
+    * @param node
+    *           the {@link Node} to serialize
+    * @param prettyPrint
+    *           if pretty-printing should be applied
+    * @return the serialized XML {@link String}
+    * @throws ParserConfigurationException
+    *            if the serializer could not be created
+    */
    public static String serialize(final Node node, final boolean prettyPrint) throws ParserConfigurationException
    {
       final DOMImplementationLS domImpl = (DOMImplementationLS) getDOMImplementation();
@@ -210,6 +369,19 @@ public class DOMUtils
       return serializer.writeToString(node);
    }
 
+   /**
+    * Serializes a given XML {@link Node} into a {@link Writer} with optionally
+    * pretty-printing.
+    * 
+    * @param node
+    *           the {@link Node} to serialize
+    * @param writer
+    *           the {@link Writer} where the {@link Node} should be serialized
+    * @param prettyPrint
+    *           if pretty-printing should be applied
+    * @throws ParserConfigurationException
+    *            if the serializer could not be created
+    */
    public static void serialize(final Node node, final Writer writer, final boolean prettyPrint)
          throws ParserConfigurationException
    {
@@ -224,15 +396,81 @@ public class DOMUtils
       serializer.write(node, output);
    }
 
+   /**
+    * Serializes a given XML {@link Node} into a {@link OutputStream} with
+    * optionally pretty-printing.
+    * 
+    * @param node
+    *           the {@link Node} to serialize
+    * @param outputStream
+    *           the {@link OutputStream} where the {@link Node} should be
+    *           serialized
+    * @param prettyPrint
+    *           if pretty-printing should be applied
+    * @throws ParserConfigurationException
+    *            if the serializer could not be created
+    */
    public static void serialize(final Node node, final OutputStream outputStream, final boolean prettyPrint)
          throws ParserConfigurationException
    {
       serialize(node, new OutputStreamWriter(outputStream), prettyPrint);
    }
 
-   public static Element addElement(final Element parent, final String tagName)
+   /**
+    * Adds a new child {@link Element} of the given namespaceUri, prefix and
+    * local name to a given parent {@link Element}.
+    * 
+    * @param parent
+    *           the parent {@link Element} where to add a new child
+    * @param namespaceUri
+    *           the namespace URI for the new {@link Element}
+    * @param prefix
+    *           the prefix for the new {@link Element}
+    * @param localName
+    *           the local name for the new {@link Element}
+    * @return the new child {@link Element}
+    */
+   public static Element addElement(final Element parent, final String namespaceUri, final String prefix,
+         final String localName)
    {
-      final Element child = parent.getOwnerDocument().createElement(tagName);
+      final Element child = parent.getOwnerDocument().createElementNS(namespaceUri,
+            NameUtils.buildQualifiedName(prefix, localName));
+
+      parent.appendChild(child);
+
+      return child;
+   }
+
+   /**
+    * Adds a new child {@link Element} of the given namespaceUri and local name
+    * to a given parent {@link Element}.
+    * 
+    * @param parent
+    *           the parent {@link Element} where to add a new child
+    * @param namespaceUri
+    *           the namespace URI for the new {@link Element}
+    * @param localName
+    *           the local name for the new {@link Element}
+    * @return the new child {@link Element}
+    */
+   public static Element addElement(final Element parent, final String namespaceUri, final String localName)
+   {
+      return addElement(parent, namespaceUri, null, localName);
+   }
+
+   /**
+    * Adds a new child {@link Element} of the given local name to a given parent
+    * {@link Element}.
+    * 
+    * @param parent
+    *           the parent {@link Element} where to add a new child
+    * @param localName
+    *           the local name (tag name) for the new {@link Element}
+    * @return the new child {@link Element}
+    */
+   public static Element addElement(final Element parent, final String localName)
+   {
+      final Element child = parent.getOwnerDocument().createElement(localName);
 
       parent.appendChild(child);
 
