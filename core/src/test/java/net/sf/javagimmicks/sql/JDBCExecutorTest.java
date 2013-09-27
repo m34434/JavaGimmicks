f738356c78a23b1c35aca0cf3a7b14f20dbb1b30
m34434 <m34434@googlemail.com>
2013-09-27 11:33:18 +0200

    New module 'jdbc-testing' and added a test for JDBCExecutor

---
+++ core/src/test/java/net/sf/javagimmicks/sql/JDBCExecutorTest.java
@@ -0,0 +1,161 @@
+package net.sf.javagimmicks.sql;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import net.sf.javagimmicks.sql.JDBCExecutor.CommandException;
+import net.sf.javagimmicks.sql.JDBCExecutor.PreparedStatementCommand;
+import net.sf.javagimmicks.sql.JDBCExecutor.PreparedStatementQueryCommand;
+import net.sf.javagimmicks.sql.JDBCExecutor.StatementCommand;
+import net.sf.javagimmicks.sql.testing.HsqlDbTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class JDBCExecutorTest
+{
+   @Rule
+   public HsqlDbTestRule _db = new HsqlDbTestRule();
+
+   private JDBCExecutor _jdbc;
+
+   @Before
+   public void setup()
+   {
+      _jdbc = new JDBCExecutor(_db.getDataSource());
+   }
+
+   @Test
+   public void createTable() throws Exception
+   {
+      _jdbc.withStatementExecute(CREATE_TABLE_FOO);
+   }
+
+   @Test
+   public void insertValues() throws Exception
+   {
+      createTable();
+      _jdbc.withPreparedStatementExecute(InsertFooCommand.SQL, new InsertFooCommand(1, "Foo", 2, "Bar"));
+   }
+
+   @Test
+   public void selectValues() throws Exception
+   {
+      insertValues();
+
+      final List<FooEntry> entries = _jdbc.withPreparedStatementQueryExecute(ExtractFooEntriesCommand.SQL,
+            new ExtractFooEntriesCommand());
+
+      assertEquals(2, entries.size());
+      assertEquals(1, entries.get(0).getKey());
+      assertEquals(2, entries.get(1).getKey());
+      assertEquals("Foo", entries.get(0).getValue());
+      assertEquals("Bar", entries.get(1).getValue());
+   }
+
+   private static final StatementCommand<Void> CREATE_TABLE_FOO = new StatementCommand<Void>() {
+      @Override
+      public Void perform(final Connection connection, final Statement statement) throws CommandException,
+            SQLException
+      {
+         statement.execute("CREATE TABLE foo(key INTEGER, value VARCHAR(255))");
+         return null;
+      }
+   };
+
+   private static class InsertFooCommand implements PreparedStatementCommand<Void>
+   {
+      public static final String SQL = "INSERT INTO foo VALUES(?, ?)";
+
+      private final Object[] _args;
+
+      public InsertFooCommand(final Object... args)
+      {
+         this._args = args;
+      }
+
+      @Override
+      public Void perform(final Connection connection, final PreparedStatement statement) throws CommandException,
+            SQLException
+      {
+         for (int cmdIndex = 0; cmdIndex < _args.length / 2; ++cmdIndex)
+         {
+            statement.setInt(1, (Integer) _args[cmdIndex * 2]);
+            statement.setString(2, (String) _args[cmdIndex * 2 + 1]);
+
+            statement.executeUpdate();
+         }
+
+         return null;
+      }
+   }
+
+   private static class ExtractFooEntriesCommand implements PreparedStatementQueryCommand<List<FooEntry>>
+   {
+      public static final String SQL = "SELECT * FROM foo";
+
+      @Override
+      public List<FooEntry> perform(final Connection connection, final PreparedStatement statement,
+            final ResultSet resultSet)
+            throws CommandException, SQLException
+      {
+         final List<FooEntry> result = new ArrayList<FooEntry>();
+
+         while (resultSet.next())
+         {
+            result.add(FooEntry.fromResultSet(resultSet));
+         }
+
+         Collections.sort(result);
+
+         return result;
+      }
+   }
+
+   private static class FooEntry implements Comparable<FooEntry>
+   {
+      private final int _key;
+      private final String _value;
+
+      public static FooEntry fromResultSet(final ResultSet resultSet) throws SQLException
+      {
+         return new FooEntry(resultSet.getInt(1), resultSet.getString(2));
+      }
+
+      public FooEntry(final int key, final String value)
+      {
+         this._key = key;
+         this._value = value;
+      }
+
+      public int getKey()
+      {
+         return _key;
+      }
+
+      public String getValue()
+      {
+         return _value;
+      }
+
+      @Override
+      public int compareTo(final FooEntry o)
+      {
+         if (o == null)
+         {
+            return Integer.MIN_VALUE;
+         }
+
+         return this.getKey() - o.getKey();
+      }
+   }
+}
---
+++ jdbc-testing/src/main/java/net/sf/javagimmicks/sql/testing/HsqlDbTestRule.java
@@ -0,0 +1,154 @@
+package net.sf.javagimmicks.sql.testing;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
+
+/**
+ * A JUnit {@link TestRule} that creates a temporary file-based Hypersonic SQL
+ * database during test execution and wraps a Commons-DBCP
+ * {@link BasicDataSource} around it.
+ * <p>
+ * The developer can influence the temporary folder, where the database should
+ * be created and/or can provide initial configuration logic for the used
+ * {@link BasicDataSource} via a {@link DataSourceConfigurator}.
+ */
+public class HsqlDbTestRule extends ExternalResource
+{
+   private final TemporaryFolder _folderRule;
+   private final DataSourceConfigurator _configurator;
+
+   private BasicDataSource _dataSource;
+
+   /**
+    * Creates a new instance.
+    * 
+    * @param configurator
+    *           a {@link DataSourceConfigurator} for performing custom
+    *           configuration logic on the internal {@link BasicDataSource}
+    * @param dbFolder
+    *           the temporary folder where the database should be set up
+    */
+   public HsqlDbTestRule(final DataSourceConfigurator configurator, final File dbFolder)
+   {
+      _configurator = configurator;
+      _folderRule = new TemporaryFolder(dbFolder);
+   }
+
+   /**
+    * Creates a new instance.
+    * 
+    * @param configurator
+    *           a {@link DataSourceConfigurator} for performing custom
+    *           configuration logic on the internal {@link BasicDataSource}
+    */
+   public HsqlDbTestRule(final DataSourceConfigurator configurator)
+   {
+      this(configurator, null);
+   }
+
+   /**
+    * Creates a new instance.
+    * 
+    * @param dbFolder
+    *           the temporary folder where the database should be set up
+    */
+   public HsqlDbTestRule(final File dbFolder)
+   {
+      this(null, dbFolder);
+   }
+
+   /**
+    * Creates a new instance.
+    */
+   public HsqlDbTestRule()
+   {
+      this(null, null);
+   }
+
+   /**
+    * Returns the {@link DataSource} representing the temporary Hypersonic SQL
+    * database.
+    * 
+    * @return the {@link DataSource} representing the temporary Hypersonic SQL
+    *         database
+    */
+   public DataSource getDataSource()
+   {
+      return _dataSource;
+   }
+
+   /**
+    * Retrieves a new {@link Connection} to the temporary Hypersonic SQL
+    * database.
+    * 
+    * @return a new {@link Connection} to the temporary Hypersonic SQL database
+    */
+   public Connection getConnection() throws SQLException
+   {
+      return getDataSource().getConnection();
+   }
+
+   @Override
+   protected void before() throws Throwable
+   {
+      _folderRule.create();
+      final File tempFolder = _folderRule.getRoot();
+
+      _dataSource = new BasicDataSource();
+      _dataSource.setDriverClassName(org.hsqldb.jdbcDriver.class.getName());
+      _dataSource.setUrl(buildJdbcUrl(tempFolder));
+      _dataSource.setUsername("sa");
+      _dataSource.setPassword("");
+
+      if (_configurator != null)
+      {
+         _configurator.configure(_dataSource);
+      }
+   }
+
+   @Override
+   protected void after()
+   {
+      try
+      {
+         _dataSource.close();
+      }
+      catch (final SQLException e)
+      {
+      }
+
+      _dataSource = null;
+
+      _folderRule.delete();
+   }
+
+   private static String buildJdbcUrl(final File tempFolder) throws MalformedURLException
+   {
+      final String url = "jdbc:hsqldb:" + tempFolder.toURI().toURL() + "test-db";
+      return url;
+   }
+
+   /**
+    * A command interface for providing custom configuration logic for the
+    * internal {@link BasicDataSource}.
+    */
+   public static interface DataSourceConfigurator
+   {
+      /**
+       * Configures the {@link BasicDataSource} used for testing
+       * 
+       * @param dataSource
+       *           the {@link BasicDataSource} to configure
+       */
+      void configure(BasicDataSource dataSource);
+   }
+}
