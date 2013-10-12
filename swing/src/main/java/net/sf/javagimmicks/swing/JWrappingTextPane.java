8d0be0fb90b2cc64887f0a5783cce73e72eb20a3
michael <m34434@googlemail.com>
2013-10-12 20:18:06 +0200

    JavaDoccing in swing module

--- swing/src/main/java/net/sf/javagimmicks/swing/JWrappingTextPane.java
+++ swing/src/main/java/net/sf/javagimmicks/swing/JWrappingTextPane.java
@@ -4,43 +4,78 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.text.StyledDocument;
 
+/**
+ * An extension of {@link JTextPane} that can be configured to line-wrap the
+ * contained text.
+ */
 public class JWrappingTextPane extends JTextPane
 {
-    private static final long serialVersionUID = -5932297203397634342L;
+   private static final long serialVersionUID = -5932297203397634342L;
 
-    private boolean _wrappingEnabled = false;
-    
-    public JWrappingTextPane()
-    {
-        super();
-    }
+   private boolean _wrappingEnabled = false;
 
-    public JWrappingTextPane(StyledDocument doc)
-    {
-        super(doc);
-    }
-    
-    public boolean isWrappingEnabled()
-    {
-        return _wrappingEnabled;
-    }
+   /**
+    * Creates a new instance.
+    * 
+    * @see JTextPane#JTextPane()
+    */
+   public JWrappingTextPane()
+   {
+      super();
+   }
 
-    public void setWrappingEnabled(boolean wrappingEnabled)
-    {
-        _wrappingEnabled = wrappingEnabled;
-    }
+   /**
+    * Creates a new instance for the given {@link StyledDocument}.
+    * 
+    * @param doc
+    *           the {@link StyledDocument} that this instance should work on
+    * @see JTextPane#JTextPane(StyledDocument)
+    */
+   public JWrappingTextPane(final StyledDocument doc)
+   {
+      super(doc);
+   }
 
-    @Override
-    public boolean getScrollableTracksViewportWidth()
-    {
-        return _wrappingEnabled ? (super.getScrollableTracksViewportWidth()) : false;
-    }
-    
-    public JScrollPane buildScrollPane()
-    {
-       JScrollPane result = new JScrollPane(this);
-       result.getViewport().setBackground(this.getBackground());
-       
-       return result;
-    }
+   /**
+    * Returns if line-wrapping is enabled
+    * 
+    * @return if line-wrapping is enabled
+    */
+   public boolean isWrappingEnabled()
+   {
+      return _wrappingEnabled;
+   }
+
+   /**
+    * Sets if line-wrapping is enabled
+    * 
+    * @param wrappingEnabled
+    *           if line-wrapping is enabled
+    */
+   public void setWrappingEnabled(final boolean wrappingEnabled)
+   {
+      _wrappingEnabled = wrappingEnabled;
+   }
+
+   @Override
+   public boolean getScrollableTracksViewportWidth()
+   {
+      return _wrappingEnabled ? (super.getScrollableTracksViewportWidth()) : false;
+   }
+
+   /**
+    * Creates a new {@link JScrollPane} that contains this
+    * {@link JWrappingTextPane} as component and additionally adjusts the
+    * background color of the {@link JScrollPane} to that of this instance which
+    * looks much better in {@link #setWrappingEnabled(boolean) wrapped} mode.
+    * 
+    * @return the resulting {@link JScrollPane}
+    */
+   public JScrollPane buildScrollPane()
+   {
+      final JScrollPane result = new JScrollPane(this);
+      result.getViewport().setBackground(this.getBackground());
+
+      return result;
+   }
 }
