7fd661e1e426e1bd3b96d9ceb45ab6cab89430f5
michael <m34434@googlemail.com>
2013-10-12 11:35:38 +0200

    Refactorings in minor updates in math module

--- math/src/main/java/net/sf/javagimmicks/math/NumberCompareUtils.java
+++ math/src/main/java/net/sf/javagimmicks/math/NumberCompareUtils.java
@@ -1,20 +1,47 @@
 package net.sf.javagimmicks.math;
 
+import java.util.Comparator;
+
 import net.sf.javagimmicks.math.comparator.ByteComparator;
 import net.sf.javagimmicks.math.comparator.LongComparator;
 
+/**
+ * A shortcut helper for comparing non-{@code int}s in a {@link Comparator}
+ * style. Internally uses {@link Comparator}s from
+ * {@link net.sf.javagimmicks.comparator} package.
+ */
 public class NumberCompareUtils
 {
    private NumberCompareUtils()
    {}
 
-   public static int compareLong(final long long1, final long long2)
+   /**
+    * Compares two {@code long}s in {@link Comparator} style.
+    * 
+    * @param n1
+    *           the first {@code long} to compare
+    * @param n2
+    *           the second {@code long} to compare
+    * @return the comparison result as {@code int}
+    * @see LongComparator
+    */
+   public static int compareLong(final long n1, final long n2)
    {
-      return new LongComparator().compare(long1, long2);
+      return new LongComparator().compare(n1, n2);
    }
 
-   public static int compareBye(final byte byte1, final byte byte2)
+   /**
+    * Compares two {@code byte}s in {@link Comparator} style.
+    * 
+    * @param n1
+    *           the first {@code byte} to compare
+    * @param n2
+    *           the second {@code byte} to compare
+    * @return the comparison result as {@code int}
+    * @see ByteComparator
+    */
+   public static int compareByte(final byte n1, final byte n2)
    {
-      return new ByteComparator().compare(byte1, byte2);
+      return new ByteComparator().compare(n1, n2);
    }
 }
